5682713d5126cf26e18e41cab193a63df98b4389
TasyaHarwati <79096171+TasyaHarwati@users.noreply.github.com>
2021-03-08 07:42:31 -0800

    Create LAB-5-Kontrol Perulangan.py

---
+++ LAB-5-Kontrol Perulangan.py
@@ -0,0 +1,30 @@
+#Tasya Setyo Harwati
+#71200580
+
+#Soal 1
+'''sebagai seorang mahasiswa Teknik informatika kamu diminta untuk membuat
+program untuk mencetak angka 1 sampai dengan 20 dengan kenaikan nilai 3
+angka'''
+#judul = 'Mencetak Angka'
+#print('~~~~~~~~~~~~~~~ Mencetak Angka ~~~~~~~~~~~~~~~')
+#i = 20
+#for i in range(1,20,3):
+    #print("%d "%i,end='\n\n\n\n\n\n')
+
+judul = 'Mencetak Angka'
+print('~~~~~~~~~~~~~~~~ Mencetak Angka ~~~~~~~~~~~~~~~~~~~')
+i = 20
+for i in range(1,20,3):
+    print("%d "%i,end='\n\n\n\n\n\n\n')
+
+    
+#Soal 2
+'''sebagai seorang mahasiswa Teknik Informatika kamu diminta untuk membuat
+program untuk menampilkan huruf AWAS CORONA dengan setiap huruf pisah tidak
+gabung saat diesekusi'''
+judul = 'Menampilkan Huruf'
+print('========== Menampilkan Huruf ==========')
+for i in 'AWAS CORONA':   
+   print (i)
+
+
