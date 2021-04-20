95e8bbf73df76439fc10e3be9f453f18f49726db
TasyaHarwati <79096171+TasyaHarwati@users.noreply.github.com>
2021-04-20 21:32:33 +0700

    Add files via upload

---
+++ LAB-9- List.py
@@ -0,0 +1,24 @@
+#Tasya Setyo Harwati
+#71200580
+
+#Soal 1
+'''Seorang mahasiswa teknik informatika sedang iseng. ia berfikir ingin
+membuat list berupa nama orang yang ia temui. tetapi ia ingin menghapus
+2 nama orang yang ia tidak suka di dalam list nama yang telah ia buat.
+buatlah program menggunakan tipe data list'''
+
+list_nama = ['Timothy','Beni','Lina','Dedy','Tomang']
+list_nama.pop(3)
+list_nama.pop(0)
+print(list_nama)
+
+#Soal 2
+'''Seorang mahasiswa teknik informatika sedang mengisi waktu luang ingin
+membuat list berupa nama buah - buahan tetapi ia ingin menduplikat nama
+buah - buahan yang sudah dibuat tadi'''
+
+buah = ['mangga','nanas','kiwi','srikaya','anggur']
+buahtotal = ['mangga','nanas','kiwi','srikaya','anggur']*4
+print(buahtotal)
+
+
