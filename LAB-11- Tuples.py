23a6eafde126aca4c5c416cb3f1fa235d86424d4
TasyaHarwati <79096171+TasyaHarwati@users.noreply.github.com>
2021-05-04 09:38:14 +0700

    Add files via upload

---
+++ LAB-11- Tuples.py
@@ -0,0 +1,37 @@
+#71200580
+#Tasya Setyo Harwati
+
+#Soal1
+'''Buatlah program yang dapat melakukan proses Membuat atau mencetak tuple dan
+menghapus tuple dengan contoh : ('mangga','Durian','Nangka','Jambu')
+Buah[0]: mangga, Buah[3]: Jambu, Buah[2]: Nangka'''
+
+Buah = ('mangga','Durian','Nangka','Jambu')
+print("Buah: ", Buah)
+print("Buah[0]: ", Buah[0])
+print("Buah[3]: ", Buah[3])
+print("Buah[2]: ", Buah[2])
+print()
+
+tuple = ('Dody', 'suka bakso', 1997)
+print(tuple)
+del tuple
+
+tup = ('Gina', 'Suka ayam', 1998)
+print("Setelah hapus tuple :", tup)
+
+#Soal2
+'''Kamu sedang mengisi waktu luang, kamu ingin membuat program yang dapat
+menghitung jumlah nilai dalam suata tipe data'''
+Tb = ('175','160','180','157','176')
+print(len(Tb))
+
+#Soal3
+'''kamu sedang mengisi waktu luang, kamu ingin membuat program yang dapat
+mengetahui false atau true data tersebut'''
+data = ('indonesia','German','Belanda','Korea selatan','Inggris','Rusia')
+print('Afrika' not in data)
+print('Belanda' in data)
+print('Jepang' in data)
+
+
