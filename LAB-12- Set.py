f49b1c379bc8d4ca6f4a191ae69cfe1650c418ea
TasyaHarwati <79096171+TasyaHarwati@users.noreply.github.com>
2021-05-11 23:21:11 +0700

    Add files via upload

---
+++ LAB-12- Set.py
@@ -0,0 +1,24 @@
+#Tasya Setyo Harwati
+#71200580
+
+#soal1
+'''kamu sedang bosan dan kamu ingin membuat operasi bilangan gabungan dengan
+tipe data set'''
+
+data1 = {2,3,4,5}
+data2 = {2,3,6,8}
+print(data1|data2)
+data3 = data1.union(data2)
+print (data3)
+
+#soal2
+'''kamu sedang bosan ingin menambahkan data dengan tipe data set'''
+
+data = {'e','f','g','h',2,3,4,5}
+print (data)
+ 
+data.add(10)
+print(data)
+ 
+data.update([12,13,14])
+print(data)
